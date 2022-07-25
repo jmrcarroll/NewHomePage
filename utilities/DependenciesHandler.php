3da5855296158fc3ab1ba6deea3c614a569a11bb
jmrcarroll <carrolljohnmr@gmail.com>
2022-07-25 21:58:30 +0100

    Abstraction class to just handle imports of JS and CSS

---
+++ utilities/DependenciesHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+class DependenciesHandler
+{
+
+    public static function Bootstrap()
+    {
+        return "<link rel='stylesheet' type='text/css' href='/Dependencies/bootstrap/css/bootstrap.css'>
+            <script type='javascript' src='/Dependencies/bootstrap/js/bootstrap.bundle.js'></script>
+            <script type='module'>import * as bootstrap from '/Dependencies/bootstrap/js/bootstrap.bundle.js';window.bootstrap = bootstrap;</script>";
+    }
+
+    public static function requireJS()
+    {
+        return "<script src='/Dependencies/requireja/require.js'></script>";
+    }
+
+    public static function jQuery()
+    {
+        return "<script src='/Dependencies/jquery/jquery-3.6.0.min.js'></script>";
+    }
+    public static function jQueryUI()
+    {
+        return "<link rel=\"stylesheet\" href=\"/Dependencies/jquery-ui-1.13.1/jquery-ui.min.css\">
+<script src='/Dependencies/jquery-ui-1.13.1/jquery-ui.min.js'></script>";
+    }
+    public static function tinymce()
+    {
+        return '<script src="/Dependencies/tinymce/tinymce.min.js" referrerpolicy="origin"></script>';
+    }
+
+    public static function DocumentStyling()
+    {
+        return "<script src='/Dependencies/js/document.js'></script>".
+            "<link rel='stylesheet' type='text/css' href='/Dependencies/css/Document.css'>";
+    }
+}

1bf7e833b94a9d136c71092191aca7fa45c43d4d
jmrcarroll <carrolljohnmr@gmail.com>
2022-08-24 21:14:41 +0100

    Filled out page with basic info

--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,17 @@
 <?php
+$title = "Admin | John Carroll";
+$NavOption = "Admin";
+include_once($_SERVER['DOCUMENT_ROOT']."/utilities/DependenciesHandler.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/template/header.php");
+?>
+    <div class="container">
+        <div class="mb-auto">
+            <div class="row">
+                <p class="h2">AdminSection</p>
+            </div>
+        </div>
+    </div>
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . "/template/footer.php");
+?>

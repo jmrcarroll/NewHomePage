b4d16fd2fdf15b3e1126dd706f8079493143bdb9
jmrcarroll <carrolljohnmr@gmail.com>
2022-08-25 19:11:54 +0100

    Pages for the kanban storyboard

---
+++ projects/Storyboard/view.php
@@ -0,0 +1,7 @@
+<?php
+$title = "Storyboard | Admin | John Carroll";
+$NavOption = "Project";
+include_once($_SERVER['DOCUMENT_ROOT']."/template/header.php");
+
+
+include_once($_SERVER['DOCUMENT_ROOT']."/template/storyboard.php");

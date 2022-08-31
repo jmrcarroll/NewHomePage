a9c67b0ee452c56a9b5899d64d61b4f863757f0e
jmrcarroll <carrolljohnmr@gmail.com>
2022-08-31 20:46:01 +0100

    formatting of form for login to the admin page

--- admin/login.php
+++ admin/login.php
@@ -1,13 +1,32 @@
 <?php
-$title = "Admin | John Carroll";
-$NavOption = "Admin";
-include_once($_SERVER['DOCUMENT_ROOT']."/utilities/DependenciesHandler.php");
+$title = "Login | Admin | John Carroll";
+
+include_once($_SERVER['DOCUMENT_ROOT'] . "/utilities/DependenciesHandler.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/template/header.php");
 ?>
     <div class="container">
-        <div class="mb-auto">
-            <div class="row">
-                <p class="h2">AdminSection</p>
+        <div class="mt-3 row justify-content-center">
+            <div class="col-9 text-center">
+                <p class="h2">Log in </p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-6 alert-secondary">
+                <div class="row p-2">
+                    <div class="col-12">
+                        <input class="form-control" placeholder="Username">
+                    </div>
+                </div>
+                <div class="row p-2">
+                    <div class="col-12">
+                        <input class="form-control" placeholder="Password" type="password">
+                    </div>
+                </div>
+                <div class="row justify-content-end p-2">
+                    <div class="col-3">
+                        <input class="btn btn-primary" type="submit" value="Login">
+                    </div>
+                </div>
             </div>
         </div>
     </div>

28c2dceb2ba9be33de1782a018538fefb967b429
jmrcarroll <carrolljohnmr@gmail.com>
2022-08-31 20:49:10 +0100

    removed needless whitespace

--- admin/projectStoryboard.php
+++ admin/projectStoryboard.php
@@ -1,8 +1,6 @@
 <?php
 $title = "Storyboard | Admin | John Carroll";
 include_once($_SERVER['DOCUMENT_ROOT']."/template/admin_header.php");
-
-
 include_once($_SERVER['DOCUMENT_ROOT']."/template/storyboard.php");
 ?>
 <script>

dc8109fe99ff019ed6432444fc201980b76d6abc
jmrcarroll <carrolljohnmr@gmail.com>
2022-09-01 22:19:08 +0100

    reworked form to actually display as intended.

--- admin/login.php
+++ admin/login.php
@@ -5,30 +5,34 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/utilities/DependenciesHandler.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/template/header.php");
 ?>
     <div class="container">
-        <div class="mt-3 row justify-content-center">
-            <div class="col-9 text-center">
+        <div class="mt-5 justify-content-center">
+            <div class="col-12 text-center">
                 <p class="h2">Log in </p>
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-6 alert-secondary">
-                <div class="row p-2">
-                    <div class="col-12">
-                        <input class="form-control" placeholder="Username">
+            <div class="col-6">
+                <div class="alert alert-secondary align-self-center mx-auto container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-6 pb-2">
+                            <input class="form-control" placeholder="Username">
+                        </div>
                     </div>
-                </div>
-                <div class="row p-2">
-                    <div class="col-12">
-                        <input class="form-control" placeholder="Password" type="password">
+                    <div class="row justify-content-center pb-2">
+                        <div class="col-6 pt-2">
+                            <input class="form-control" placeholder="Password" type="password">
+                        </div>
                     </div>
-                </div>
-                <div class="row justify-content-end p-2">
-                    <div class="col-3">
-                        <input class="btn btn-primary" type="submit" value="Login">
+                    <div class="row justify-content-center pt-2">
+                        <div class="col-6">
+                            <button type="submit" name="submit" class="btn btn-primary float-end">Login</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
+
     </div>
 
 <?php

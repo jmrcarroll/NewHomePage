10ca59c90f7ff3435b985b8ce2e0c1af495c3f87
jmrcarroll <carrolljohnmr@gmail.com>
2022-09-23 22:57:04 +0100

    started design for kanban storyboard and breaking into three main sections.

--- template/storyboard.php
+++ template/storyboard.php
@@ -1,1 +1,23 @@
 <?php
+
+?>
+
+
+<div class="container-fluid">
+    <div class="row justify-content-center mt-2">
+        <div class="col-12 text-center">Queued</div>
+        <div class="col-12 text-center">In progress</div>
+        <div class="col-12 text-center">Completed</div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+    </div>
+</div>

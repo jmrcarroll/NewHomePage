4d1114c9faf79dd306e45990aab94556f8a81cf3
jmrcarroll <carrolljohnmr@gmail.com>
2022-11-02 10:52:50 +0000

    Added constructor and functions to generate from parent object ID and Database row.

--- Classes/SubTask.php
+++ Classes/SubTask.php
@@ -2,11 +2,71 @@
 
 class SubTask
 {
+    private $ID;
+    private $TaskID;
+    private $title;
+    private $description;
+
+    protected function __construct($array)
+    {
+        if(isset($array['id'])) $this->ID = $array['id'];
+        $this->TaskID = $array['Task_id'];
+        $this->title = $array['title'];
+        $this->description = $array['desc'];
+    }
 
     public static function getAllSubTaskByTaskID($taskID)
     {
-        $subtasks = [];
+        $DB = new Databases();
+        $subtasks = $DB->execMainQuery();
 
-        return $subtasks;
+        $subtaskList = [];
+        foreach ($subtasks as $subtask){
+            $subtaskList[] = self::fromDBRow($subtask);
+        }
+        return $subtaskList;
+    }
+
+    public static function fromSTID($SubTaskID) :self
+    {
+        $DB = new Databases();
+        $array = $DB->execMainQuerySingleRes();
+        return self::fromDBRow($array);
+    }
+    public static function fromDBRow($array) : self
+    {
+        return new self($array);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTaskID()
+    {
+        return $this->TaskID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
     }
 }

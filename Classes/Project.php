0cb8461b5afb293d48962c31e929d1e52e5e3a3f
jmrcarroll <carrolljohnmr@gmail.com>
2022-11-02 10:55:43 +0000

    Scaffolded class fro construction functions and likely gets needed to properly display the page.

--- Classes/Project.php
+++ Classes/Project.php
@@ -5,9 +5,49 @@ class Project
     private $ID;
     private $title;
     private $tasks;
+    private $token;
     private $updates;
     private $languages;
 
+
+    private function __construct($array = array())
+    {
+        if(isset($array['ID'])) {
+            $this->ID = $array['ID'];
+            $DB = new Databases();
+            $languages = $DB->execMainQuery("SELECT *,Languages.* FROM `Project-language-link`  left join Languages on Languages.ID = `Project-language-link`.`ID` where ProjectID = {$this->ID} AND `removed` = 0");
+            $this->languages = [];
+            foreach ($languages as $language){
+                $this->languages[] = $language['LanguageName'];
+            }
+
+
+        }
+        if(isset($array['title'])) $this->title = $array['title'];
+        if(isset($array['slug'])) $this->token = $array['slug'];
+        if(isset($array['ID'])) $this->tasks = Task::fromProjectID($this->ID);
+        if(!isset($array['ID'])) $this->tasks = array();
+        if(isset($array['ID'])) $this->updates = ProjectUpdate::fromProjectID($this->ID);
+        if(!isset($array['ID'])) $this->updates = array();
+
+    }
+
+    public static function fromID()
+    {
+
+    }
+
+    public static function fromArray($array)
+    {
+        return new self($array);
+    }
+
+    //simplification in case to save calling {Class}->title
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return $this->title;
+    }
 
     /**
      * @return mixed
@@ -39,6 +79,14 @@ class Project
     public function getTasks()
     {
         return $this->tasks;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
     }
 
     /**
@@ -80,6 +128,14 @@ class Project
     public function setTasks($tasks): void
     {
         $this->tasks = $tasks;
+    }
+
+    /**
+     * @param mixed $token
+     */
+    public function setToken($token): void
+    {
+        $this->token = $token;
     }
 
     /**

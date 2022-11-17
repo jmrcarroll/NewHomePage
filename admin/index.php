6103e991b98bc4eeab774d76f95fd2a935f65e14
jmrcarroll <carrolljohnmr@gmail.com>
2022-11-17 21:39:19 +0000

    basic design for admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -3,10 +3,54 @@ $title = "Home | Admin | John Carroll";
 
 include_once($_SERVER['DOCUMENT_ROOT'] . "/template/admin_header.php");
 ?>
-    <div class="container">
-        <div class="mb-auto">
-            <div class="row">
-                <p class="h2">Blog</p>
+
+    <div class="container-fluid container-lg">
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header text-center" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Blog Posts
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="container-fluid">
+                                    <div class="row justify-content-center">
+                                        <div class="col-6 text-center">
+                                            test
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header text-center" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Projects
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header text-center" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Curriculum Vitale
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
